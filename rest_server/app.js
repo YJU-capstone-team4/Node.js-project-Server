9263545589d76e744debb1c7dcfca49df3bb3d55
Onean <go05117@naver.com>
2021-02-05 20:03:58 +0900

    Feature :: 로그인 1차 완성

--- rest_server/app.js
+++ rest_server/app.js
@@ -6,12 +6,12 @@ const session       = require('express-session');
 const mongoose      = require('mongoose');
 const cors          = require('cors');
 
-// 관리자 로컬 로그인용
-const LocalStrategy = require('passport-local').Strategy;
-const cookieSession = require('cookie-session');
-const flash         = require('connect-flash');
-
 const PORT = 3000;
+
+// 관리자용 로그인
+const LocalStrategy = require('passport-local').Strategy
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
 
 // DB 연결
 mongoose.connect(process.env.ATLAS_URI, 
@@ -26,15 +26,6 @@ connection.once('open', () => {
 app.set('view engine', 'ejs');
 app.use(session({secret:'MySecret', resave: false, saveUninitialized:true}));
 
-// 관리자 로그인용 쿠키-세션 세팅
-app.use(cookieSession({
-  keys: ['node_yun'],
-  cookie: {
-    maxAge: 1000 * 60 * 60 // 유효기간 1시간
-  }
-}));
-app.use(flash());
-
 // passport 세팅
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,6 +36,39 @@ app.get('/', (req, res) => {
     });
 });
 
+const AdminTb = require('./models/adminTb.model');
+
+passport.use(new LocalStrategy({ // local 전략을 세움
+  usernameField: 'adminId',
+  passwordField: 'adminPw',
+  session: true, // 세션에 저장 여부
+  passReqToCallback: false,
+}, (adminId, adminPw, done) => {
+AdminTb.findOne({ userId: adminId }, async (findError, user) => {
+  const pw = await AdminTb.findOne({userId: adminId});
+  if (adminId == pw.userId && adminPw == pw.password)
+      return done(null, user); // 검증 성공
+})
+}))
+
+passport.serializeUser((user, done) => { // Strategy 성공 시 호출됨
+  done(null, user); // 여기의 user가 deserializeUser의 첫 번째 매개변수로 이동
+});
+
+passport.deserializeUser((user, done) => { // 매개변수 user는 serializeUser의 done의 인자 user를 받은 것
+  done(null, user); // 여기의 user가 req.user가 됨
+});
+
+app.post('/admin/login', passport.authenticate('local', {
+  failureRedirect: '/login', failureFlash: true
+}), // 인증 실패 시 401 리턴, {} -> 인증 스트레티지
+  function (req, res) {
+    // res.redirect('/home');
+    res.json({
+      success: true,
+    });
+});
+
 // // 로그인 라우트
 // app.use('/', require('./routes/main'));
 // app.use('/auth', require('./routes/auth'));
@@ -52,7 +76,7 @@ app.get('/', (req, res) => {
 app.use(cors());
 app.use(express.json());
 
-const loginRouter = require('./routes/db/login');
+const loginRouter = require('./routes/db/passport');
 const userTbRouter = require('./routes/db/userTb');
 const adminTbRouter = require('./routes/db/adminTb');
 const adminTagTbRouter = require('./routes/db/adminTagTb');
